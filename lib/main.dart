d5b6e986c1facce54747f74d3fd17265b761a2de
Himanshu Godara <6560138+HimanshuGodara@users.noreply.github.com>
2025-10-08 22:07:33 +0530

    added category tag

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'dart:math' show Random;
 
 import 'package:flutter/material.dart';
+import 'package:todo_app/entities/category_entity.dart';
+
 import 'package:todo_app/models/local_category_model.dart';
-import 'package:todo_app/repository/category_repo_impl.dart';
+
 import 'package:uuid/uuid.dart';
 
 void main() {
@@ -15,41 +17,73 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            spacing: 24,
-            children: [
-              Text('Hello World!'),
-              FilledButton(
-                onPressed: () async {
-                  print('button pressed');
-                  // add button =>
-                  // ✅ generate a random category and
-                  // ✅ add to the file
-                  final randomCategory = createRandomCategory();
-                  print('random category created as: \n$randomCategory');
-                  print(
-                    'random category created as: \n${randomCategory.toString()}',
-                  );
-                  print(
-                    'random category created as: \n${randomCategory.toMap()}',
-                  );
-                  print(
-                    'random category created as: \n${randomCategory.toMap().toString()}',
-                  );
-                  // final categoryRepo = CategoryRepoImpl();
-
-                  // await categoryRepo.createCategory(category: randomCategory);
-                  // print('create category callback has finished');
-                },
-                child: Text('Add a random category'),
-              ),
-            ],
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              // crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text('Hello World!'),
+                ListTile(
+                  onTap: () {},
+                  title: Text('Tile title'),
+                  leading: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    spacing: 4,
+                    children: [
+                      ColoredBox(
+                        color: Colors.amber,
+                        child: SizedBox(width: 4, height: 24),
+                      ),
+                      Icon(Icons.check_circle, color: Colors.green),
+                    ],
+                  ),
+                ),
+                CategoryTag(category: createRandomCategory()),
+                CategoryTag(category: createRandomCategory()),
+                CategoryTag(category: createRandomCategory()),
+                CategoryTag(category: createRandomCategory()),
+                CategoryTag(category: createRandomCategory()),
+                CategoryTag(category: createRandomCategory()),
+                CategoryTag(category: createRandomCategory()),
+                CategoryTag(category: createRandomCategory()),
+                CategoryTag(category: createRandomCategory()),
+              ],
+            ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class CategoryTag extends StatelessWidget {
+  const CategoryTag({super.key, required this.category});
+  final CategoryEntity category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 249, 250, 251),
+        border: Border.all(color: Color.fromARGB(255, 229, 231, 235)),
+        borderRadius: BorderRadius.circular(4),
+      ),
+      padding: EdgeInsets.all(4),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        spacing: 8,
+        children: [
+          ColoredBox(
+            color: category.color,
+            child: SizedBox(width: 4, height: 24),
+          ),
+          Text(category.name),
+        ],
       ),
     );
   }
